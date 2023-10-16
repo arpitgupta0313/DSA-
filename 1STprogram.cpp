d9470dc6b77f04d2fd673028ea96495760c7dfaa
arpitgupta0313 <144698614+arpitgupta0313@users.noreply.github.com>
2023-10-16 20:01:36 +0530

    Add files via upload

---
+++ 1STprogram.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"namaste duniya";
+    int a ;
+    char b = 'a';
+    bool c = true;
+    double d= 4.5;
+    a= cin.get();
+    cout<<endl<<a<<endl<<b<<endl<<c<<endl<<d;
+}
